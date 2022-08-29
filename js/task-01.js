904594715691ca6cc7bbc84914c342a1f535172e
AlinaShklyaruk <i.charge92@gmail.com>
2022-08-29 15:55:02 +0300

    First task is done

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,11 @@
-const listItems = document.querySelectorAll(".item");
-const message = `Number of categories: ${listItems.length}`;
+const categoryList = document.querySelectorAll(".item");
+const message = `Number of categories: ${categoryList.length}`;
 console.log(message);
+
+[...categoryList].forEach(function (element, index) {
+    const categoryTitle = element.querySelector("h2");
+    const categoryItems = element.querySelectorAll("li");
+    console.log(`Category: ${categoryTitle.textContent}`);
+    console.log(`Elements: ${categoryItems.length}`);
+});
+
