50655d97024b28b16f0918fd05fd43c959eec4f2
AlinaShklyaruk <i.charge92@gmail.com>
2022-09-07 14:36:18 +0300

    Update task-06.js

---
+++ js/task-06.js
@@ -0,0 +1,8 @@
+const inputEl = document.querySelector("validation-input");
+const min
+
+inputEl.addEventListener('focus', onInput);
+function onInput(event) {
+    const inputValue = event.currentTarget.value;
+    if (inputValue.length ===)
+}
