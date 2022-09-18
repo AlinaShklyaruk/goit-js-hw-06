9db41425ee4c93ab84d05cb8d7c2772296f6f00a
AlinaShklyaruk <i.charge92@gmail.com>
2022-09-18 22:47:03 +0300

    8th task is done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const formEl = document.querySelector('.login-form');
+
+
+formEl.addEventListener('submit', onSubmit);
+function onSubmit(event) {
+    event.preventDefault();
+
+
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    const elements = { email, password };
+
+    if (email === '' || password === '') {
+        alert('Все поля должны быть заполнены. Пожалуйста, заполните поля');
+    } else {
+        console.log(elements);
+        event.currentTarget.reset();
+    }
+        
+}
 
