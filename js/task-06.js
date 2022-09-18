b3310f93005f74371262fd980216c0bf9c150c26
AlinaShklyaruk <i.charge92@gmail.com>
2022-09-18 18:51:17 +0300

    6th task is done

--- js/task-06.js
+++ js/task-06.js
@@ -1,10 +1,15 @@
 const inputEl = document.querySelector("#validation-input");
-const minLength = inputEl.getAttribute("data-length");
+const minLength = parseInt(inputEl.getAttribute("data-length"));
 
 inputEl.addEventListener('blur', onInput);
+
 function onInput(event) {
     const inputValue = event.currentTarget.value;
-    inputValue.length === minLength ? inputEl.classList.add("valid") : inputEl.classList.add("invalid");
+    if (inputValue.length === minLength) {
+        inputEl.classList.add('valid');
+        inputEl.classList.remove('invalid');
+    } else {
+        inputEl.classList.add('invalid');
+        inputEl.classList.remove('valid');
+    }
 }
-console.log(minLength);
-console.log(inputValue.length);
