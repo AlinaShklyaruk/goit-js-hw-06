b021aca357a52621ca42a7e5e01c36a0804be3e8
AlinaShklyaruk <i.charge92@gmail.com>
2022-09-20 15:47:00 +0300

    9th task is done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const buttonEl = document.querySelector('button.change-color');
+const colorEl = document.querySelector('span.color');
+const bodyEl = document.querySelector('body');
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+buttonEl.addEventListener('click', changeColor);
+
+function changeColor() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  colorEl.textContent = `${bodyEl.style.backgroundColor}`;
 }
