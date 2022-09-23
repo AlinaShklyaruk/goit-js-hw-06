d45b104872b5e3486b6cb4f8d5da6eef01eec809
AlinaShklyaruk <i.charge92@gmail.com>
2022-09-24 00:23:57 +0300

    Started task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,19 @@
+const createEl = document.querySelector('data-create');
+const deleteEl = document.querySelector('data-destroy');
+
+createEl.addEventListener('click', onCreateBtn);
+deleteEl.addEventListener('click', onDestroyBtn);
+
+function onCreateBtn() {
+  const boxEl = document.createElement('div');
+  boxEl.backgroundColor = getRandomHexColor();
+
+
+}
+
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
