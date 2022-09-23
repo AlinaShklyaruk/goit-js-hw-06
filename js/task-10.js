3b1969204d2e0579d4b2c449943400e961fc9fbb
AlinaShklyaruk <i.charge92@gmail.com>
2022-09-24 01:09:16 +0300

    Update task-10.js
    input path need to change

--- js/task-10.js
+++ js/task-10.js
@@ -1,14 +1,25 @@
-const createEl = document.querySelector('data-create');
-const deleteEl = document.querySelector('data-destroy');
+const createEl = document.querySelector('button[data-create]');
+const deleteEl = document.querySelector('button[data-destroy]');
+const inputEl = document.querySelector('input type="number"');
+const boxesEl = document.querySelector('#boxes');
 
-createEl.addEventListener('click', onCreateBtn);
+
+
+const numberOfBoxes = inputEl.value;
+
+console.log(numberOfBoxes);
+
+createEl.addEventListener('click', onCreateBtn(numberOfBoxes));
 deleteEl.addEventListener('click', onDestroyBtn);
 
-function onCreateBtn() {
-  const boxEl = document.createElement('div');
-  boxEl.backgroundColor = getRandomHexColor();
-
-
+function onCreateBtn(numberOfBoxes) {
+  for (let i = 0; i <= numberOfBoxes; i += 1) {
+    const boxEl = document.createElement('div');
+    boxEl.backgroundColor = getRandomHexColor();
+    boxEl.width = `${30 + i * 10}px`;
+    boxEl.height = `${30 + i * 10}px`;
+    boxesEl.appendChild(boxEl);
+  }
 }
 
 
